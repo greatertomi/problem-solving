1e2b978f398e1fd7f680d92710bb9915570f4050
Oshalusi John <oshalusijohn@gmail.com>
2020-11-10 22:10:56 +0100

    Minor work done

--- algorithms/easy2/seperate-the-numbers.py
+++ algorithms/easy2/seperate-the-numbers.py
@@ -1,26 +1,81 @@
 # Problem Link: https://www.hackerrank.com/challenges/separate-the-numbers/problem
 
 
-def separateNumbers(string):
-    length = len(string)
-    if length < 2:
-        return 'NO'
-
-    status = 'NO'
+def testing(string):
     width = 2
     i = 0
     nums = []
-    while i < length:
+    while i < len(string):
         num = string[i: i + width]
-        print(num)
-        i += width
+        nums.append(num)
+        if int(nums[-1][-1]) == 9:
+            width += len(nums[-1])
+            i += len(nums[-1])
+        else:
+            i += width
+        try:
+            if int(num) - int(nums[-2]) != 1:
+                status = 'NO'
+                break
+            else:
+                status = 'YES'
+        except IndexError:
+            pass
+    print(nums)
 
 
-string1 = '1234'
-string2 = '91011'
-string3 = '99100'
-string4 = '101103'
-string5 = '010203'
-string6 = '13'
-string7 = '1'
-print(separateNumbers(string2))
+def checkForZero(arr):
+    for num in arr:
+        if int(num[0]) == 0:
+            return True
+
+    return False
+
+
+def separateNumbers(string):
+    length = len(string)
+    minNum = None
+    if length < 2:
+        print('NO')
+    else:
+        status = 'NO'
+        for i in range(1, length//2 + 1):
+            width = i
+            i = 0
+            nums = []
+            while i < length:
+                num = string[i: i + width]
+                nums.append(num)
+                if int(nums[-1][-1]) == 9:
+                    width += len(nums[-1])
+                    i += len(nums[-1])
+                else:
+                    i += width
+                try:
+                    if int(num) - int(nums[-2]) != 1:
+                        status = 'NO'
+                        break
+                    else:
+                        status = 'YES'
+                except IndexError:
+                    pass
+            if status == 'YES' and checkForZero(nums) is False:
+                minNum = nums[0]
+                break
+            print(nums)
+        if status == 'YES':
+            print(status, minNum)
+        else:
+            print(status)
+
+
+string1 = '99910001001'
+string2 = '7891011'
+string3 = '9899100'
+string4 = '999100010001'
+
+testing(string2)
+# separateNumbers(string1)
+# separateNumbers(string2)
+# separateNumbers(string3)
+# separateNumbers(string4)
